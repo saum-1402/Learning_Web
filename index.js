74cf25ebade5d6a934790dc3e295ea16b63e3216
Saumil <saumil21097@iiitd.ac.in>
2024-01-10 14:33:24 +0530

    uname & password

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 3000
+// const bodyParser = require('body-parser')
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+const usernames = []
+const passwords = []
+
+//this is the get the html file from the server
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname+'/index.html')
+})
+
+//this is to give the 
+app.post('/', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(username)
+  console.log(password)
+  usernames.push(username)
+  passwords.push(password)
+  
+  res.send('Form filled')
+
+  res.redirect('/welcome')
+})
+
+app.get('/welcome', (req, res) => {
+  res.send('Welcome')
+
 })
 
 app.listen(port, () => {
